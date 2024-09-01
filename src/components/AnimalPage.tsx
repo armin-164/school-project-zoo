a9dc9bbb155230b76269d4fc35d152ac4dbb36c2
armin-164 <arminsadr164@hotmail.com>
2024-09-01 21:50:06 +0200

    Implement AnimalPage component to display details of a selected animal

--- src/components/AnimalPage.tsx
+++ src/components/AnimalPage.tsx
@@ -1,9 +1,34 @@
+import { useParams } from "react-router-dom"
 import { Header } from "./Header"
+import { useState } from "react";
+import { IAnimal } from "../models/IAnimal";
 
 export const AnimalPage = () => {
+    const storedAnimals = JSON.parse(localStorage.getItem("animals") || "[]");
+
+    const [animals, setAnimals] = useState<IAnimal[]>([...storedAnimals]);
+  
+    const params = useParams();
+  
+    const foundAnimal = animals.find(
+      (animal) => animal.id.toString() === params.id
+    );
+
     return (
         <>
         <Header></Header>
+        {!foundAnimal ? (
+            <p>Animal not found</p>
+        ) : (
+            <>
+            <h3>{foundAnimal.name}</h3>
+            <span>{foundAnimal.latinName}</span>
+            <img src={foundAnimal.imageUrl} alt={foundAnimal.latinName} className="animal-img"/>
+            <p>{foundAnimal.longDescription}</p>
+            <p>{foundAnimal.lastFed}</p>
+            <button>Feed</button>
+            </>
+        )}
         </>
     )
 }
