5b9a7f4f1c688abdbb54350f9fe08f7ba3c4ce7c
armin-164 <arminsadr164@hotmail.com>
2024-09-01 17:30:00 +0200

    Add error handling for get request

--- src/services/zooService.ts
+++ src/services/zooService.ts
@@ -3,7 +3,18 @@ import { IAnimal } from "../models/IAnimal";
 const BASE_URL = "https://animals.azurewebsites.net/api/animals";
 
 export const getData = async (): Promise<IAnimal[]> => {
-    const response = await fetch(BASE_URL);
-    const result: IAnimal[] = await response.json();
-    return result;
+    try {
+        const response = await fetch(BASE_URL);
+
+        if (!response.ok) {
+            throw new Error(`Error code: ${response.status}`);
+        }
+
+        const result: IAnimal[] = await response.json();
+        return result;
+
+    } catch (error) {
+        console.error("Failed to fetch data:", error);
+        return [];
+    }
 };
